377ef8c1a2d78c9e2c72b6d4070ed8f876b7fb67
shabil15 <mohammedshabil15@gmail.com>
2024-02-16 10:33:37 +0530

    first commit

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <h1 class="text-4xl font-bold text-blue-600">Header</h1>
+      
+    </>
+  )
+}
+
+export default Header
