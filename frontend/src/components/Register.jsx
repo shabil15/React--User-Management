9b4b916387fe070796f1412a1a64bbb8cccef047
shabil15 <mohammedshabil15@gmail.com>
2024-02-17 09:53:38 +0530

    Register page ui done

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -22,6 +22,18 @@ function Register() {
             <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
             <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
           </div>
+          <div>
+            <label htmlFor="password" className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Password</label>
+            <input type='password' name='password' id='password'  placeholder="••••••••" className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' required="" ></input>
+          </div>
+          <div>
+            <label htmlFor="password" className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>Confirm Password</label>
+            <input type='password' name='password' id='password'  placeholder="••••••••" className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' required="" ></input>
+          </div>
+          <button type="submit" className="w-full text-white bg-emerald-500 hover:bg-emerald-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                  <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                      Already have an account ? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-cyan-500">Login</a>
+                  </p>
         </form>
       </div>
 
