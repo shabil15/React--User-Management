c18a6d4e62b240fceceb99bdcf6faaf3ea349b17
shabil15 <mohammedshabil15@gmail.com>
2024-02-19 17:19:59 +0530

    login page made right

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -5,7 +5,7 @@ function Register() {
   <section className='bg-gray-50 dark:bg-gray-900' >
     <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
       <a href="#" className='flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white'>
-      <img className="w-8 h-8 mr-2" src="public\Group 31.png" alt="logo"/>
+      <img className="w-8 h-8 mr-2" src="/Group 31.png" alt="logo"/>
           Opus Blitz
         </a> 
     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark dark:border-gray-700">
